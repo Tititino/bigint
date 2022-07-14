61228d9d0db2b27ac7d99f2dabedd03e03a0dbe6
Tititino <59182246+Tititino@users.noreply.github.com>
2022-07-14 19:46:02 +0200

    Update big_int.h

--- big_int.h
+++ big_int.h
@@ -1,10 +1,6 @@
 #ifndef BIG_INT_H
 #define BIG_INT_H
 
-#ifndef BASE
-#define BASE 	(unsigned long long) 10000000
-#endif
-
 typedef struct bigInt* BigInt;
 typedef unsigned long long Chunk;
 
@@ -14,6 +10,7 @@ void print_big_int(BigInt n);
 BigInt multiply_big_int(BigInt a, BigInt b);
 char* big_int_to_string(BigInt n);
 void destroy_big_int(BigInt n);
+int big_int_change_def_base(Chunk);
 
 
 #endif
